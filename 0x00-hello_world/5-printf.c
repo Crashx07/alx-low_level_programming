2db2fafa604e74c25ff8942802d9b4518a5fc52b
David Davou <95164584+Crashx07@users.noreply.github.com>
2022-12-12 03:23:05 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main - main code block
- * return: 0
- */
+* main - Entry point
+*
+* Returns: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
