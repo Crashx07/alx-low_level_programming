d449d0681251df723ae644b8c93ef8ab9c2812e3
David <davoudavid8@gmail.com>
2022-12-12 02:37:41 +0100

    betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 * main - main code block
-*returns: 0
+* returns: int 0
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
