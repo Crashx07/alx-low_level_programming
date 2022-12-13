19a1b59ea0f3c7177559ffbc7746ef1e9785f6cd
David <davoudavid8@gmail.com>
2022-12-12 19:04:10 -0800

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -main code block
+ * Description: Prints all numbers between 0 and 9
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%i", a);
+		a++;
+	}
+	return (0);
+}
