bb237924fda0789f338b380f33cc9f8dfa561ee4
David <davoudavid8@gmail.com>
2022-12-14 17:58:58 -0800

    4 -isaplha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,3 +6,13 @@
  * Return: int 1 if c is a letter or int 0 if not
  */
 int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
