992dfdea1a762750bbab1df881a95085275a6e63
David <davoudavid8@gmail.com>
2022-12-14 18:12:49 -0800

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * _isalpha - thisfunction checks is a character is an aplhabet
+ * @c: variable for a single letter
+ * Return: int 1 id c is a letter or int 0 if not
+ */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
