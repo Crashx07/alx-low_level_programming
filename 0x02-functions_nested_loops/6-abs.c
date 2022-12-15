0c0e540aeb4ae5b4e93067c3d9dcf45f722f6111
David <davoudavid8@gmail.com>
2022-12-14 18:59:55 -0800

    absolut value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - calculates and prints out the absolute value of an int
+ * @i: interger input
+ * Return: absolut value of a
+ */
+int _abs(int a)
+{
+	int b;
+
+	b = abs(a);
+	return b;
+}
