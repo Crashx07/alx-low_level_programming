08172650f734606e9fd459a30f19c712694a855d
David <davoudavid8@gmail.com>
2022-12-15 12:16:49 -0800

    is upper update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,13 @@
 #include "main.h"
 /**
- * _isupper - Checksif a character is in upper case
- * @c: stores the value of a character
+ * _isupper - a functio to find if a char is uppercase
+ * @c: stores char
  * Return: 1 if uppercase and 0 if not
  */
 int _isupper(int c)
 {
-
-
 	if (isupper(c))
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
