b752aeaae7fe39e7e3ff8e29a21c2e429efc3f65
Crashx07 <davoudavid8@gmail.com>
2022-12-23 02:17:19 +0100

    test c

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - test if code snippets work
+ * Return: 0 (depending on the code snippet)
+ */
+int main(void)
+{
+	int a, b;
+
+	a = 0;
+	for (a = 0; a < i; a++)
+	{
+		printf("%d is the numer?" a)
+	}
+}
